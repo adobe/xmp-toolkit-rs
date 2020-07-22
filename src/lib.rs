4c54a1ea99db18f8a7a02b04f571f2b2a7ec7640
Eric Scouten <scouten@adobe.com>
2020-07-22 10:30:32 -0700

    Reorgnize mod and use statements.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-mod ffi;
-mod xmp_const;
-mod xmp_date_time;
-mod xmp_file;
-mod xmp_meta;
+#![deny(warnings)]
 
+mod ffi;
+
+mod xmp_const;
 pub use xmp_const::*;
+
+mod xmp_date_time;
 pub use xmp_date_time::XmpDateTime;
+
+mod xmp_file;
 pub use xmp_file::OpenFileOptions;
 pub use xmp_file::XmpFile;
 pub use xmp_file::XmpFileError;
+
+mod xmp_meta;
 pub use xmp_meta::XmpMeta;
