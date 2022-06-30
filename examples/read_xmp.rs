176281be22022f9061c2d5c22a51b9e163db2252
Eric Scouten <scouten@adobe.com>
2022-06-30 11:05:52 -0500

    Fix up code formatting
    Also remove eprintln I was using for debugging.

--- examples/read_xmp.rs
+++ examples/read_xmp.rs
@@ -31,7 +31,6 @@ use std::env;
 
 fn read_xmp_from_file() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    eprintln!("args = {:#?}", args);
 
     let path = match args.len() {
         // args[0] = path to executable
@@ -44,17 +43,22 @@ fn read_xmp_from_file() -> Result<()> {
 
     let mut f = XmpFile::new();
 
-    f.open_file(&path, OpenFileOptions::OPEN_ONLY_XMP | OpenFileOptions::OPEN_USE_SMART_HANDLER)
-        .or_else(|_err| {
-            eprintln!(
-                "No smart handler available for file {}. Trying packet scanning.",
-                path
-            );
-            f.open_file(path, OpenFileOptions::OPEN_USE_PACKET_SCANNING)
-        })
-        .with_context(|| format!("could not find XMP in file {}", path))?;
+    f.open_file(
+        &path,
+        OpenFileOptions::OPEN_ONLY_XMP | OpenFileOptions::OPEN_USE_SMART_HANDLER,
+    )
+    .or_else(|_err| {
+        eprintln!(
+            "No smart handler available for file {}. Trying packet scanning.",
+            path
+        );
+        f.open_file(path, OpenFileOptions::OPEN_USE_PACKET_SCANNING)
+    })
+    .with_context(|| format!("could not find XMP in file {}", path))?;
 
-    let _xmp = f.xmp().ok_or(anyhow!("unable to process XMP in file {}", path))?;
+    let _xmp = f
+        .xmp()
+        .ok_or(anyhow!("unable to process XMP in file {}", path))?;
 
     // TODO: Continue from step 10 of C++ example.
 
