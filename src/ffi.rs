7e0af7ca8ccff91556f7f0e1434f61486da9337a
Eric Scouten <scouten@adobe.com>
2022-07-07 12:31:11 -0700

    (MINOR) Hide access to FFI functions (#61)
    These were never intended to be public in any way.

--- src/ffi.rs
+++ src/ffi.rs
@@ -13,51 +13,51 @@
 
 use std::os::raw::{c_char, c_int};
 
-pub enum CXmpDateTime {}
-pub enum CXmpFile {}
-pub enum CXmpMeta {}
+pub(crate) enum CXmpDateTime {}
+pub(crate) enum CXmpFile {}
+pub(crate) enum CXmpMeta {}
 
 extern "C" {
     // --- CXmpFile ---
 
-    pub fn CXmpFileNew() -> *mut CXmpFile;
-    pub fn CXmpFileDrop(file: *mut CXmpFile);
-    pub fn CXmpFileOpen(file: *mut CXmpFile, path: *const c_char, flags: u32) -> c_int;
-    pub fn CXmpFileClose(file: *mut CXmpFile);
-    pub fn CXmpFileGetXmp(file: *mut CXmpFile) -> *mut CXmpMeta;
-    pub fn CXmpFilePutXmp(file: *mut CXmpFile, meta: *const CXmpMeta);
-    pub fn CXmpFileCanPutXmp(file: *const CXmpFile, meta: *const CXmpMeta) -> c_int;
+    pub(crate) fn CXmpFileNew() -> *mut CXmpFile;
+    pub(crate) fn CXmpFileDrop(file: *mut CXmpFile);
+    pub(crate) fn CXmpFileOpen(file: *mut CXmpFile, path: *const c_char, flags: u32) -> c_int;
+    pub(crate) fn CXmpFileClose(file: *mut CXmpFile);
+    pub(crate) fn CXmpFileGetXmp(file: *mut CXmpFile) -> *mut CXmpMeta;
+    pub(crate) fn CXmpFilePutXmp(file: *mut CXmpFile, meta: *const CXmpMeta);
+    pub(crate) fn CXmpFileCanPutXmp(file: *const CXmpFile, meta: *const CXmpMeta) -> c_int;
 
     // --- CXmpMeta ---
 
-    pub fn CXmpMetaNew() -> *mut CXmpMeta;
-    pub fn CXmpMetaDrop(meta: *mut CXmpMeta);
+    pub(crate) fn CXmpMetaNew() -> *mut CXmpMeta;
+    pub(crate) fn CXmpMetaDrop(meta: *mut CXmpMeta);
 
-    pub fn CXmpMetaRegisterNamespace(
+    pub(crate) fn CXmpMetaRegisterNamespace(
         namespace_uri: *const c_char,
         suggested_prefix: *const c_char,
     ) -> *mut c_char;
 
-    pub fn CXmpMetaGetProperty(
+    pub(crate) fn CXmpMetaGetProperty(
         meta: *mut CXmpMeta,
         schema_ns: *const c_char,
         prop_name: *const c_char,
     ) -> *mut c_char;
 
-    pub fn CXmpMetaSetProperty(
+    pub(crate) fn CXmpMetaSetProperty(
         meta: *mut CXmpMeta,
         schema_ns: *const c_char,
         prop_name: *const c_char,
         prop_value: *const c_char,
     );
 
-    pub fn CXmpMetaDoesPropertyExist(
+    pub(crate) fn CXmpMetaDoesPropertyExist(
         meta: *const CXmpMeta,
         schema_ns: *const c_char,
         prop_name: *const c_char,
     ) -> c_int;
 
-    pub fn CXmpMetaSetPropertyDate(
+    pub(crate) fn CXmpMetaSetPropertyDate(
         meta: *mut CXmpMeta,
         schema_ns: *const c_char,
         prop_name: *const c_char,
@@ -66,7 +66,7 @@ extern "C" {
 
     // --- CXmpDateTime ---
 
-    pub fn CXmpDateTimeNew() -> *mut CXmpDateTime;
-    pub fn CXmpDateTimeDrop(dt: *mut CXmpDateTime);
-    pub fn CXmpDateTimeCurrent() -> *mut CXmpDateTime;
+    pub(crate) fn CXmpDateTimeNew() -> *mut CXmpDateTime;
+    pub(crate) fn CXmpDateTimeDrop(dt: *mut CXmpDateTime);
+    pub(crate) fn CXmpDateTimeCurrent() -> *mut CXmpDateTime;
 }
