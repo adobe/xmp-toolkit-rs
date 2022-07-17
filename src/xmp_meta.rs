5e7bc7aa681723f3ab887bc0c5de7bd052cd880b
Eric Scouten <scouten@adobe.com>
2022-07-16 22:11:08 -0700

    Introduce new error type `XmpErrorType::NulInRustString` (#68)
    Use this to remove some `.unwrap()` calls in `XmpMeta`.

--- src/xmp_meta.rs
+++ src/xmp_meta.rs
@@ -158,9 +158,9 @@ impl XmpMeta {
         prop_name: &str,
         prop_value: &str,
     ) -> XmpResult<()> {
-        let c_ns = CString::new(schema_ns).unwrap();
-        let c_name = CString::new(prop_name).unwrap();
-        let c_value = CString::new(prop_value).unwrap();
+        let c_ns = CString::new(schema_ns)?;
+        let c_name = CString::new(prop_name)?;
+        let c_value = CString::new(prop_value)?;
         let mut err = ffi::CXmpError::default();
 
         unsafe {
@@ -196,8 +196,8 @@ impl XmpMeta {
         prop_name: &str,
         prop_value: &XmpDateTime,
     ) -> XmpResult<()> {
-        let c_ns = CString::new(schema_ns).unwrap();
-        let c_name = CString::new(prop_name).unwrap();
+        let c_ns = CString::new(schema_ns)?;
+        let c_name = CString::new(prop_name)?;
         let mut err = ffi::CXmpError::default();
 
         unsafe {
