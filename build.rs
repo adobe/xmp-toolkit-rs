68ad472067c3e249ad71b6bc2b8cd08e4fab5110
Eric Scouten <scouten@adobe.com>
2024-07-07 15:25:30 -0700

    Redo debug stuff

--- build.rs
+++ build.rs
@@ -351,7 +351,7 @@ fn copy_external_to_third_party(from_path: &str, to_path: &str) {
     src_path.push("external");
     src_path.push(from_path);
 
-    eprintln!("Copying external lib from {}", src_path.display());
+    dbg!(&src_path);
     assert!(src_path.is_dir());
 
     dest_path.pop();
